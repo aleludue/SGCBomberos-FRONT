bef874ef105280c434c24f547ffd0593b7595b24
ALE-PC\Ale <aleludue@hotmail.com>
2025-12-31 17:40:29 -0300

    1-Instalacion y configuracion inicial de PWA para el front.

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,34 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['LogoPrincipal.png', 'LogoCuartel.png'],
+      manifest: {
+        name: 'SGCB PWA',
+        short_name: 'SGCBApp',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'LogoPrincipal.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'LogoPrincipal.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
