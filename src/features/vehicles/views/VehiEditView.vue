2bad26840052d7c146d88f50bdec642c17bc89ed
ALE-PC\Ale <aleludue@hotmail.com>
2026-07-19 20:05:41 -0300

    Fix para deploy de frront en ambiente por vista sin configurar..

---
+++ src/features/vehicles/views/VehiEditView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <SectionTitle
+      :title="t('VehiclesViews.EditTitle')"
+      :subtitle="t('VehiclesViews.EditSubtitle')"
+      :breadcrumb="true"
+      :breadcrumbDetail="[
+        { detail: t('VehiclesViews.MenuTitle'), link: '/vehicles' },
+        { detail: t('VehiclesViews.ConsultTitle'), link: '/vehicles/consult' },
+        { detail: t('VehiclesViews.EditTitle') },
+      ]"
+    />
+
+    <BtnBack :toHome="false" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+import BtnBack from '@/shared/components/Button/BtnBack.vue';
+import SectionTitle from '@/shared/components/SectionTitle.vue';
+import { useSiteConfigStore } from '@/shared/stores/config.store';
+
+const { desactivateSpinner } = useSiteConfigStore();
+const { t } = useI18n();
+
+onMounted(() => {
+  desactivateSpinner();
+});
+</script>
